631fb399017b28f2b423f1f02c18f931bd5621df
tryTo <w@tryto.cn>
2020-05-08 10:46:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
+
 // Module to control application life.
 const app = electron.app;
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
